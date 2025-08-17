b09ef0d45df64770c29e0f182a181da2f503fa1f
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2025-08-17 16:28:12 -0600

    style(ui): improve homepage layout and colors

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ function App() {
   return (
     <>
     <header className='header'>
-      <h1>Pokedéx</h1>
+      <h1 className='header-title'>Pokedéx 3ra Generación</h1>
     </header>
 
 
     <div className='main-container'>
       {Array.from({ length: 135 }).map((_, index) => (
         <PokemonCard key={index} id={index+252} />
-        // <PokemonCard key={index} id={25} />
       ))}
     </div>
     </>
